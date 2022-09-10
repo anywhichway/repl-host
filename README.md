8fce8c3f607fc850711b545f4c65f4e0844f3e6f
syblackwell <syblackwell@anywhichway.com>
2022-09-10 03:47:50 -0700

    Added documentation and load improvements

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # repl-host
 A web component for hosting REPLs
+
+# Usage
+
+`@anywhichway/repl-host` is designed to be loaded and used from a CDN like https://www.jsdelivr.com. It is also designed
+to be loaded and instantiated by `@anywhichway/quick-component`.
+
+Insert this line into your HTML file:
+
+```html
+<script src="https://cdn.jsdelivr.net/npm/@anywhichway/quick-component.js@0.0.5" component="https://cdn.jsdelivr.net/npm/@anywhichway/repl-host@0.0.3"></script>
+```
+
+Version numbers are used above to insulate your use from unexpected changes due to future enhancements. You can also use
+the most recent version of the software with the code below:
+
+```html
+<script src="https://cdn.jsdelivr.net/npm/@anywhichway/quick-component.js" component="https://cdn.jsdelivr.net/npm/@anywhichway/repl-host"></script>
+```
+
+After this you can include the tag `<repl-host>` in your HTML.
+
+# Configuration
+
+A `repl-host` is configured with both attributes and slots. The simplest configuration is to automatically create an
+empty REPL that supports head, css, body, and javascript editing using attributes with non-enumerated values.
+
+```html
+<repl-host head css body javascript></repl-host>
+```
+
+By leaving out an attribute, you will prevent the use of the named REPL section. The below is a REPL for playing with 
+CSS.
+
+```html
+<repl-host css body></repl-host>
+```
+
+You can provide initial content for any section of the REPL using slots:
+
+```html
+<repl-host head css body javascript>
+    <slot name="css">h1 { font-size:small}</slot>
+    <slot name="body">&lt;h1>Test&lt;/h1></slot>
+</repl-host>
+```
+
+# Security
+
+REPLs are run in iframes. You can configure the iframe security using the same attributes used by an iframe, i.e.
+`allow`, `allowfullscren`, `allowpaymentrequest`, `referrerpolicy`, `csp`, `sandbox`. See 
+https://developer.mozilla.org/en-US/docs/Web/HTML/Element/iframe.
+
+# Version History (reverse chronological order)
+
+2022-09-10 v0.0.3 Added documentation and load improvements
--- index.js
+++ index.js
@@ -1,171 +1,208 @@
-(() => {
-    const self = window.currentComponent;
+const self = window.currentComponent(import.meta.url);
 
-    self.properties({
-        connected() {
-            this.setAttribute("contenteditable","false");
-            this.cursor = document.createElement("cursor");
-            const allow = this.getAttribute("allow"),
-                sandbox = this.getAttribute("sandbox"),
-                iframe = this.shadowRoot.querySelector("iframe");
-            if(allow!=null) iframe.setAttribute("allow",allow);
-            if(sandbox!=null) iframe.setAttribute("sandbox",sandbox);
-            [...this.shadowRoot.querySelectorAll("pre code[slot]")].forEach((textarea) => {
-                textarea.setAttribute("spellcheck","false");
-                textarea.setAttribute("contenteditable","true");
-                textarea.addEventListener("beforeinput",(event) => {
-                    textarea.previousTextContent = textarea.textContent;
+function css_sanitize(css) {
+    const iframe = document.createElement("iframe");
+    iframe.style.display = "none";
+    iframe.style.width = "10px";
+    iframe.style.height = "10px";
+    document.body.appendChild(iframe);
+    const style = iframe.contentDocument.createElement('style');
+    style.innerHTML = css;
+    iframe.contentDocument.head.appendChild(style);
+    const sheet = style.sheet,
+        result = Array.from(style.sheet.cssRules).map(rule => rule.cssText || '').join('\n');
+    iframe.remove();
+    return result;
+}
+
+self.properties({
+    connected() {
+        this.setAttribute("contenteditable","false");
+        this.cursor = document.createElement("cursor");
+        const iframe = this.shadowRoot.querySelector("iframe");
+        ["allow","allowfullscren","allowpaymentrequest","csp","sandbox"].forEach((name) => {
+            const value = this.getAttribute(name);
+            if(value!=null) iframe.setAttribute(name,value)
+        });
+        [...this.shadowRoot.querySelectorAll("pre code[slot]")].forEach((textarea) => {
+            textarea.addEventListener("beforeinput",(event) => {
+                textarea.previousTextContent = textarea.textContent;
+            })
+            textarea.addEventListener("input",(event) => {
+                event.stopImmediatePropagation();
+                this.cursor.node = textarea;
+                if(textarea.getAttribute("slot")==="css") {
+                    textarea.normalize();
+                    const css = css_sanitize(textarea.textContent);
+                    if(css!==textarea.textContent.trim().replaceAll(/\n/g," ").replaceAll(/  +/g, " ")) {
+                        lastError = null;
+                        console.error("CSS appears invalid")
+                        return;
+                    }
+                    textarea.innerText = css;
+                }
+                const outer = textarea.textContent.length >= textarea.previousTextContent.length ? textarea.textContent : textarea.previousTextContent,
+                    inner = textarea.textContent.length > textarea.previousTextContent.length ? textarea.previousTextContent : textarea.textContent;
+                for(let i=0;i<outer.length;i++) {
+                    if(outer[i]!==inner[i]) {
+                        textarea.start = textarea.textContent.substring(0,i+1);
+                        break;
+                    }
+                }
+                const {target} = event,
+                    name = target.getAttribute("slot"),
+                    slot = this.querySelector(`slot[name="${name}"]`);
+                if(name==="javascript") {
+                    try {
+                        iframe.contentWindow.eval(target.textContent);
+                        slot.innerText = target.textContent;
+                        this.render();
+                    } catch(e) {
+                        iframe.contentWindow.console.error(e+"");
+                    }
+                } else {
+                    slot.innerText = target.textContent;
+                    this.render();
+                }
+            });
+            textarea.addEventListener("paste",(event) => {
+                event.stopImmediatePropagation();
+            })
+        });
+        let lastError;
+        const console = iframe.contentWindow.console = this.shadowRoot.getElementById("console"),
+            log = (color,...args) => {
+                const div = iframe.contentDocument.createElement("div");
+                div.style.color = color;
+                args.forEach((arg) => {
+                    const span = iframe.contentDocument.createElement("span");
+                    if(arg && typeof(arg)==="object") {
+                        try {
+                            span.innerText = JSON.stringify(arg);
+                        } catch (e) {
+                            span.innerText = arg;
+                        }
+                    } else {
+                        span.innerText = arg;
+                    }
+                    div.appendChild(span);
+                });
+                console.appendChild(div);
+            };
+        console.log = (...args) => log("black",...args);
+        console.warn = (...args) => log("orange",...args);
+        console.error = (arg) => {
+            if(arg===lastError) return;
+            lastError = arg;
+            log("red",arg);
+        }
+        console.clear = () => console.innerHTML = "";
+    },
+    render() {
+        const iframe = this.shadowRoot.querySelector("iframe"),
+            slots = {
+                head: this.querySelector('slot[name="head"]'),
+                css: this.querySelector('slot[name="css"]'),
+                body: this.querySelector('slot[name="body"]'),
+                javascript: this.querySelector('slot[name="javascript"]')
+            };
+        iframe.contentWindow.console.clear();
+        Object.entries(slots).forEach(([key,value]) => {
+            const hasSlot = this.hasAttribute(key) ? this.getAttribute(key)||true : null;
+            if(hasSlot!==null) {
+                if(!value) {
+                    value = slots[key] = document.createElement("slot");
+                    value.setAttribute("name",key);
+                    this.appendChild(value);
+                }
+            }
+            const slot = this.shadowRoot.querySelector(`[slot="${key}"]`);
+            slot.setAttribute("spellcheck","false");
+            slot.setAttribute("contenteditable","true");
+            if(value) {
+                slot.innerText = value.textContent;
+                [...value.attributes].forEach((attr) => {
+                    slot.setAttribute(attr.name,attr.value);
                 })
-                textarea.addEventListener("input",(event) => {
-                    event.stopImmediatePropagation();
-                    this.cursor.node = textarea;
-                    const outer = textarea.textContent.length >= textarea.previousTextContent.length ? textarea.textContent : textarea.previousTextContent,
-                        inner = textarea.textContent.length > textarea.previousTextContent.length ? textarea.previousTextContent : textarea.textContent;
-                    for(let i=0;i<outer.length;i++) {
-                        if(outer[i]!==inner[i]) {
-                            textarea.offset = i + 1;
+            }
+            slot.parentElement.style.display = "";
+            if(hasSlot==null) {
+                slot.parentElement.style.display = "none";
+            } else if(!hasSlot) {
+                slot.setAttribute("readonly","");
+                slot.setAttribute("disabled","");
+            }
+        })
+        iframe.contentDocument.head.innerHTML = "";
+        if(slots.head) {
+            const head = this.shadowRoot.querySelector('[slot="head"]');
+            hljs.highlightElement(head);
+            iframe.contentDocument.head.innerHTML = head.textContent;
+        }
+        if(slots.css) {
+            const css = this.shadowRoot.querySelector('[slot="css"]'),
+                style = document.createElement("style");
+            style.innerText = css.textContent;
+            hljs.highlightElement(css);
+            iframe.contentDocument.head.appendChild(style);
+        }
+        iframe.contentDocument.body.innerHTML = "";
+        if(slots.body) {
+            const body = this.shadowRoot.querySelector('[slot="body"]');
+            [...slots.body.attributes].forEach((attr) => {
+                if(attr.name!=="name") iframe.contentDocument.body.setAttribute(attr.name,attr.value);
+            });
+            hljs.highlightElement(body);
+            iframe.contentDocument.body.innerHTML = body.textContent;
+        }
+        if(slots.javascript) {
+            const javascript = this.shadowRoot.querySelector('[slot="javascript"]'),
+                script = document.createElement("script");
+            [...slots.javascript.attributes].forEach((attr) => {
+                if(attr.name!=="name") script.setAttribute(attr.name,attr.value);
+            });
+            script.innerHTML = javascript.textContent;
+            hljs.highlightElement(javascript);
+            iframe.contentDocument.body.appendChild(script);
+        }
+        if(this.cursor.node) {
+            const selection = document.getSelection();
+            selection.extend(this.cursor.node);
+            const range = selection.getRangeAt(0);
+            let offset, text = this.cursor.node.start;
+            const findNode = (node) => {
+                for(const child of node.childNodes) {
+                    node = child;
+                    if(node.textContent.length>=text.length) {
+                        if(node.childNodes.length>0) {
+                            node = findNode(node);
+                            if(text.length===0) break;
+                        } else {
+                            offset = text.length;
+                            if(this.cursor.node.previousTextContent.length>this.cursor.node.textContent.length) {
+                                --offset;
+                            }
+                        }
+                        break;
+                    }
+                    if(text.startsWith(node.textContent)) {
+                        text = text.substring(node.textContent.length);
+                        if(text.length===0) {
+                            offset = 1;
                             break;
                         }
                     }
-                    const {target} = event,
-                        name = target.getAttribute("slot"),
-                        slot = this.querySelector(`slot[name="${name}"]`);
-                    if(name==="javascript") {
-                        try {
-                            iframe.contentWindow.eval(target.textContent);
-                            slot.innerText = target.textContent;
-                            this.render();
-                        } catch(e) {
-                            iframe.contentWindow.console.error(e+"");
-                        }
-                    } else {
-                        slot.innerText = target.textContent;
-                        this.render();
-                    }
-                });
-                textarea.addEventListener("paste",(event) => {
-                    event.stopImmediatePropagation();
-                })
-            });
-            let lastError;
-            const console = iframe.contentWindow.console = this.shadowRoot.getElementById("console"),
-                log = (color,...args) => {
-                    const div = iframe.contentDocument.createElement("div");
-                    div.style.color = color;
-                    args.forEach((arg) => {
-                        const span = iframe.contentDocument.createElement("span");
-                        if(arg && typeof(arg)==="object") {
-                            try {
-                                span.innerText = JSON.stringify(arg);
-                            } catch (e) {
-                                span.innerText = arg;
-                            }
-                        } else {
-                            span.innerText = arg;
-                        }
-                        div.appendChild(span);
-                    });
-                    console.appendChild(div);
-                };
-            console.log = (...args) => log("black",...args);
-            console.warn = (...args) => log("orange",...args);
-            console.error = (arg) => {
-                if(arg===lastError) return;
-                lastError = arg;
-                log("red",arg);
-            }
-            console.clear = () => console.innerHTML = "";
-        },
-        render() {
-            const iframe = this.shadowRoot.querySelector("iframe"),
-                slots = {
-                    head: this.querySelector('slot[name="head"]'),
-                    css: this.querySelector('slot[name="css"]'),
-                    body: this.querySelector('slot[name="body"]'),
-                    javascript: this.querySelector('slot[name="javascript"]')
-                };
-            iframe.contentWindow.console.clear();
-            Object.entries(slots).forEach(([key,value]) => {
-                const hasSlot = this.hasAttribute(key) ? this.getAttribute(key)||true : null;
-                if(hasSlot!==null) {
-                    if(!value) {
-                        value = slots[key] = document.createElement("slot");
-                        value.setAttribute("name",key);
-                        this.appendChild(value);
-                    }
                 }
-                const slot = this.shadowRoot.querySelector(`[slot="${key}"]`);
-                if(value) {
-                    slot.innerText = value.textContent;
-                    [...value.attributes].forEach((attr) => {
-                        slot.setAttribute(attr.name,attr.value);
-                    })
-                }
-                slot.parentElement.style.display = "";
-                if(hasSlot==null) {
-                    slot.parentElement.style.display = "none";
-                } else if(!hasSlot) {
-                    slot.setAttribute("readonly","");
-                    slot.setAttribute("disabled","");
-                }
-            })
-            iframe.contentDocument.head.innerHTML = "";
-            if(slots.head) {
-                const head = this.shadowRoot.querySelector('[slot="head"]');
-                hljs.highlightElement(head);
-                iframe.contentDocument.head.innerHTML = head.textContent;
+                return node;
             }
-            if(slots.css) {
-                const css = this.shadowRoot.querySelector('[slot="css"]'),
-                    style = document.createElement("style");
-                style.innerText = css.textContent;
-                hljs.highlightElement(css);
-                iframe.contentDocument.head.appendChild(style);
+            const node = findNode(this.cursor.node);
+            if(node) {
+                if(offset===undefined && node.nodeType===Node.TEXT_NODE) offset = node.textContent.length;
+                range.setStart(node,offset);
             }
-            iframe.contentDocument.body.innerHTML = "";
-            if(slots.body) {
-                const body = this.shadowRoot.querySelector('[slot="body"]');
-                [...slots.body.attributes].forEach((attr) => {
-                    if(attr.name!=="name") iframe.contentDocument.body.setAttribute(attr.name,attr.value);
-                });
-                hljs.highlightElement(body);
-                iframe.contentDocument.body.innerHTML = body.textContent;
-            }
-            if(slots.javascript) {
-                const javascript = this.shadowRoot.querySelector('[slot="javascript"]'),
-                    script = document.createElement("script");
-                [...slots.javascript.attributes].forEach((attr) => {
-                    if(attr.name!=="name") script.setAttribute(attr.name,attr.value);
-                });
-                script.innerHTML = javascript.textContent;
-                hljs.highlightElement(javascript);
-                iframe.contentDocument.body.appendChild(script);
-            }
-            if(this.cursor.node) {
-                const selection = document.getSelection();
-                selection.extend(this.cursor.node);
-                const range = selection.getRangeAt(0);
-                let node, offset
-                for(const child of this.cursor.node.childNodes) {
-                    node = child;
-                    if(node.textContent.length>this.cursor.node.offset) {
-                        offset = node.textContent.length - this.cursor.node.offset;
-                        break;
-                    }
-                    if(node.textContent.length===this.cursor.node.offset) {
-                        offset = this.cursor.node.offset;
-                        break;
-                    }
-                    this.cursor.node.offset -= node.textContent.length;
-                }
-                if(node) {
-                    node = node.firstChild||node;
-                    if(offset===undefined && node.nodeType===Node.TEXT_NODE) offset = node.textContent.length;
-                    range.setStart(node,offset);
-                }
-                // loop through child node textContent up to the index
-                // set the range at the last node + remainder of index
-            }
+            // loop through child node textContent up to the index
+            // set the range at the last node + remainder of index
         }
-    })
-})();
+    }
+})
+
